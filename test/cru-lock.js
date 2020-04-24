33fd098981ffd9fee9a8ec834cd30e0f8d151149
denis.sergeev <denis.sergeev@folioltd.com>
2020-04-24 14:05:56 +0300

    Add tests for locking

--- test/cru-lock.js
+++ test/cru-lock.js
@@ -1,6 +1,6 @@
 const config = require('./../config/infeos_config.json');
 const infeos = require('infeos').init();
-const assertThrowsAsync = require('./include/functions')
+const {assertThrowsAsync, getTable} = require('./include/functions')
 const EOSIOApi = infeos.EOSIOApi;
 const EOSIORpc = infeos.EOSIOApi.rpc;
 
@@ -21,46 +21,17 @@ describe('TokenLock Contract Tests', function() {
     it("[Non-existing] - Should add user with algorithm 0 to " + config.accounts[1].name, async () => {
         let initialBalance = await EOSIORpc.get_currency_balance('eosio.token', config.accounts[1].name, 'CRU')
         await tokenLockContractInstance.add(config.accounts[1].name, 0, 0, "2020-04-08T16:11:22", 0, 1000000)
-        let historyTable = await EOSIORpc.get_table_rows({
-            json: true,               // Get the response as json
-            code: config.tokenLockContract,      // Contract that we target
-            scope: config.accounts[1].name,         // Account that owns the data
-            table: 'history',        // Table name
-            limit: 10,                // Maximum number of rows that we want to get
-            reverse: false,           // Optional: Get reversed data
-            show_payer: false          // Optional: Show ram payer
-        })
+        let historyTable = await getTable(config.tokenLockContract, config.accounts[1].name, 'history')
         let balance = await EOSIORpc.get_currency_balance('eosio.token', config.accounts[1].name, 'CRU')
         assert.strictEqual(config.accounts[1].name, historyTable.rows[0].username, 'User name  [${config.accounts[1].name}] was expected')
         assert.strictEqual(balance[0], Number.parseInt(initialBalance[0].split(" ")) + 1000000 + " CRU", 'Balance value does not match to expected one ' + balance[0])
     });
 
     it("[Non-existing] - Should add user with algorithm 1 to " + config.accounts[2].name, async () => {
-
-
-
         let initialBalance = await EOSIORpc.get_currency_balance('eosio.token', config.accounts[2].name, 'CRU')
         await tokenLockContractInstance.add(config.accounts[2].name, 0, 0, "2020-04-08T16:11:22", 1, 1000000)
-        let historyTable = await EOSIORpc.get_table_rows({
-            json: true,
-            code: config.tokenLockContract,
-            scope: config.accounts[2].name,
-            table: 'history',
-            limit: 10,
-            reverse: false,
-            show_payer: false
-        })
-
-        let lockTable = await EOSIORpc.get_table_rows({
-            json: true,
-            code: config.tokenLockContract,
-            scope: config.accounts[2].name,
-            table: 'locks',
-            limit: 10,
-            reverse: false,
-            show_payer: false
-        })
-
+        let historyTable = await getTable(config.tokenLockContract, config.accounts[2].name, 'history')
+        let lockTable = await getTable(config.tokenLockContract, config.accounts[2].name, 'locks')
         let balance = await EOSIORpc.get_currency_balance('eosio.token', config.accounts[2].name, 'CRU')
         assert.strictEqual("1000000 CRU", lockTable.rows[0].amount, 'Table lock should contains allocation of 1000000 CRU')
         assert.strictEqual(config.accounts[2].name, historyTable.rows[0].username, 'User name  [${config.accounts[2].name}] was expected')
@@ -68,8 +39,7 @@ describe('TokenLock Contract Tests', function() {
     });
 
     it("[Existing lock object] - Should not add user with algorithm 1 to " + config.accounts[2].name, async () => {
-        //TODO: Not sure we need this assert
-        assertThrowsAsync( ()=> tokenLockContractInstance.add(config.accounts[2].name, 0, 0, "2020-04-08T16:11:22", 1, 1000000),
+        await assertThrowsAsync(() => tokenLockContractInstance.add(config.accounts[2].name, 0, 0, "2020-04-08T16:11:22", 1, 1000000),
             "assertion failure with message: Lock object with current ID is already exist",
             "Lock ID is already there, should throw exception with 500 code")
     });
@@ -77,68 +47,74 @@ describe('TokenLock Contract Tests', function() {
     it("[Non-existing] - Should add user with algorithm 2 to " + config.accounts[3].name, async () => {
         let initialBalance = await EOSIORpc.get_currency_balance('eosio.token', config.accounts[3].name, 'CRU')
         await tokenLockContractInstance.add(config.accounts[3].name, 0, 0, "2020-04-08T16:11:22", 2, 1000000)
-        let historyTable = await EOSIORpc.get_table_rows({
-            json: true,
-            code: config.tokenLockContract,
-            scope: config.accounts[3].name,
-            table: 'history',
-            limit: 10,
-            reverse: false,
-            show_payer: false
-        })
+        let historyTable = await getTable(config.tokenLockContract, config.accounts[3].name, 'history')
+        let lockTable = await getTable(config.tokenLockContract, config.accounts[3].name, 'locks')
         let balance = await EOSIORpc.get_currency_balance('eosio.token', config.accounts[3].name, 'CRU')
+        assert.strictEqual("1000000 CRU", lockTable.rows[0].amount, 'Table lock should contains allocation of 1000000 CRU')
         assert.strictEqual(config.accounts[3].name, historyTable.rows[0].username, 'User name  [${config.accounts[3].name}] was expected')
         assert.strictEqual(balance[0], initialBalance[0], 'Balance value does not match to expected one ' + balance[0])
     });
 
-    it("[Non-existing] - Should add user with algorithm 0 to " + config.accounts[1].name + " and amount of non-existing token", async () => {
-        await tokenLockContractInstance.add(config.accounts[1].name, 0, 0, "2020-04-08T16:11:22", 1, 1000000)
+    it("[Existing lock parent] - Should add user with algorithm 1 to " + config.accounts[1].name + " and reduce lock balance", async () => {
+        await tokenLockContractInstance.add(config.accounts[1].name, 1, 0, "2020-04-08T16:11:22", 1, 5000)
+        let lockInitialTable = await getTable(config.tokenLockContract, config.accounts[1].name, 'locks')
 
+        await tokenLockContractInstance.add(config.accounts[1].name, 2, 1, "2020-04-08T16:11:22", 1, -500)
+        let lockTable = await getTable(config.tokenLockContract, config.accounts[1].name, 'locks')
+        assert.strictEqual(lockTable.rows[0].amount, Number.parseInt(lockInitialTable.rows[0].amount.split(" ")) - 500 +
+            " CRU", 'Balance value does not match to expected one ' + lockTable.rows[0].amount)
     });
 
-    it("[Non-existing] - Should add user with algorithm 0 to " + config.accounts[1].name + " and amount of non-existing token", async () => {
-        await tokenLockContractInstance.add(config.accounts[2].name, 0, 0, "2020-04-08T16:11:22", 0, 1000000)
+    it("[Existing lock parent] - Should throw exception for user with algorithm 1 to " + config.accounts[1].name +
+        " and tries to increase balance", async () => {
+        await assertThrowsAsync(() => tokenLockContractInstance.add(config.accounts[1].name, 2, 1, "2020-04-08T16:11:22", 1, 500),
+            "assertion failure with message: Only the ability to reduce balance is available.",
+            "Balance should be negative to reduce parent lock")
     });
 
-    it("[Migrate - Withdraw] - Should try to withdraw for " + config.accounts[1].name + " and fail", async () => {
-
-    });
-
-    it("[Migrate] - Should migrate  " + config.accounts[1].name , async () => {
-
-    });
-
-    it("[Migrate] - Check Users Table to observe migrated users ", async () => {
-
-    });
-
-    it("[Refresh] - Refresh locks for user  " + config.accounts[1].name + " and period 1" , async () => {
-
-    });
-
-    it("[Refresh] - Refresh locks for user  " + config.accounts[1].name + " and period 2" , async () => {
-
-    });
-
-    it("[Refresh] - Refresh locks for user  " + config.accounts[1].name + " and period 3" , async () => {
-
-    });
-
-    it("[Refresh] - Refresh locks for unknown user "  , async () => {
-
-    });
-
-    it("[Refresh] - Refresh locks for unknown user "  , async () => {
-
-    });
-
-    it("[Locks] - Check against lock table and user nbalance"  , async () => {
-
-    });
-
-    it("[Withdraw] - Should withdraw to " + config.accounts[1].name + " and update locks table", async () => {
-
-    });
+    // it("[Non-existing] - Should add user with algorithm 0 to " + config.accounts[1].name + " and amount of non-existing token", async () => {
+    //     await tokenLockContractInstance.add(config.accounts[2].name, 0, 0, "2020-04-08T16:11:22", 0, 1000000)
+    // });
+    //
+    // it("[Migrate - Withdraw] - Should try to withdraw for " + config.accounts[1].name + " and fail", async () => {
+    //
+    // });
+    //
+    // it("[Migrate] - Should migrate  " + config.accounts[1].name , async () => {
+    //
+    // });
+    //
+    // it("[Migrate] - Check Users Table to observe migrated users ", async () => {
+    //
+    // });
+    //
+    // it("[Refresh] - Refresh locks for user  " + config.accounts[1].name + " and period 1" , async () => {
+    //
+    // });
+    //
+    // it("[Refresh] - Refresh locks for user  " + config.accounts[1].name + " and period 2" , async () => {
+    //
+    // });
+    //
+    // it("[Refresh] - Refresh locks for user  " + config.accounts[1].name + " and period 3" , async () => {
+    //
+    // });
+    //
+    // it("[Refresh] - Refresh locks for unknown user "  , async () => {
+    //
+    // });
+    //
+    // it("[Refresh] - Refresh locks for unknown user "  , async () => {
+    //
+    // });
+    //
+    // it("[Locks] - Check against lock table and user nbalance"  , async () => {
+    //
+    // });
+    //
+    // it("[Withdraw] - Should withdraw to " + config.accounts[1].name + " and update locks table", async () => {
+    //
+    // });
 
 
 });
--- test/include/functions.js
+++ test/include/functions.js
@@ -1,11 +1,29 @@
-module.exports = (
-    async function assertThrowsAsync(test, error, details) {
+const infeos = require('infeos').init();
+const AssertionError = require('assertion-error');
+const EOSIORpc = infeos.EOSIOApi.rpc;
+
+module.exports = {
+    assertThrowsAsync: async (test, error, details) => {
         let exception
         try {
             await test();
         } catch(e) {
             exception = e;
         }
-        assert.strictEqual(exception ? exception.json.error.details[0].message : exception, error, details)
+        if((exception ? exception.json.error.details[0].message : exception) !== error)
+            throw new AssertionError("Exception:" + error + " should be raised")
+
+    },
+
+    getTable: async (code, scope, table, limit = 10) => {
+        return await EOSIORpc.get_table_rows({
+            json: true,
+            code: code,
+            scope: scope,
+            table: table,
+            limit: limit,
+            reverse: false,
+            show_payer: false
+        })
     }
-)
+}
